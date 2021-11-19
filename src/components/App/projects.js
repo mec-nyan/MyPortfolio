dfcd6f2af73be27617a7a1d200cb1bf5211d745e
mec-nyan <marianoraspberrypi@gmail.com>
2021-11-19 16:23:35 -0300

    Portfolio templates: to re-use the portfolio component

---
+++ src/components/App/projects.js
@@ -0,0 +1,21 @@
+const mecGames = (
+  <div className='templateContainer'>
+    <h2 className='templateTitle' data-aos='slide-right'>Mec Games</h2>
+    <div className='templateUnderline' data-aos='slide-left'></div>
+    <h3 className='templateSubtitle'>A videogames catalogue</h3>
+    <article>
+      <p>My first solo project with React and a RESTful API at <b>Soy Henry</b>.
+      Users can browse the catalogue for games and even add their own game to our databse including description, images, and more.</p>
+      <p>This project was developed using the following technologies:</p>
+      <h4>Front end</h4>
+      <ul>
+        <li>React, Redux, React-router</li>
+        <li>second item</li>
+        <li>yet another item</li>
+      </ul>
+    </article>
+  </div>)
+
+export {
+  mecGames,
+}
