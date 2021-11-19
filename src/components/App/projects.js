683d073e8cd55a2f062f07dd7ba44bba22d32a65
mec-nyan <marianoraspberrypi@gmail.com>
2021-11-19 19:50:37 -0300

    templates for other projects

--- src/components/App/projects.js
+++ src/components/App/projects.js
@@ -24,6 +24,81 @@ const mecGames = (
     </article>
   </div>)
 
+const waveMusic = (
+  <div className='templateContainer' data-aos='fade-up'>
+    <h2 className='templateTitle' data-aos='slide-right'>Wave Music</h2>
+    <div className='templateUnderline' data-aos='slide-left'></div>
+    <h3 className='templateSubtitle'>Henry's final project</h3>
+    <article>
+      <p>As part of the bootcamp we worked in groups during four weeks developing an e-commerce using React and agile methodologies. This project also included <b>authentication</b> with <b>Auth0</b> and payment via <b>PayPal</b>.</p>
+      <p>This project was developed using the following technologies:</p>
+      <h4>Front end</h4>
+      <ul>
+        <li>React</li>
+        <li>Redux</li>
+        <li>React-router</li>
+        <li>Styled with CSS only</li>
+        <li>Other react-related libraries</li>
+      </ul>
+      <h4>Back end</h4>
+      <ul>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>Express.js</li>
+        <li>Our own RESTful API</li>
+      </ul>
+      <p>The app was deployed via <b>Firebase</b> (front end) and <b>Heroku</b> (back end).</p>
+    </article>
+  </div>)
+
+const weatherApp = (
+  <div className='templateContainer' data-aos='fade-up'>
+    <h2 className='templateTitle' data-aos='slide-right'>Whatever the weather</h2>
+    <div className='templateUnderline' data-aos='slide-left'></div>
+    <h3 className='templateSubtitle'>My very first react app</h3>
+    <article>
+      <p>One of my favourites homeworks was this, the weather app. With this simple app we've got the chance to apply the technical skills we've learned so far.</p>
+      <p>This project was developed using the following technologies:</p>
+      <h4>Front end</h4>
+      <ul>
+        <li>React</li>
+        <li>React-router</li>
+        <li>Styled with CSS only</li>
+      </ul>
+      <h4>Back end</h4>
+      <ul>
+        <li>fetch / axios</li>
+        <li>RESTful API <b>(openweather)</b></li>
+      </ul>
+    </article>
+  </div>)
+
+const myPortfolio = (
+  <div className='templateContainer' data-aos='fade-up'>
+    <h2 className='templateTitle' data-aos='slide-right'>Resume / Portfolio</h2>
+    <div className='templateUnderline' data-aos='slide-left'></div>
+    <h3 className='templateSubtitle'>My presentation card</h3>
+    <article>
+      <p>This SPA is entirely developed in React. Part resume, part portfolio, here you can what I've learned at <b>Soy Henr</b> and some of my skills in practice.</p>
+      <p>This project was developed using the following technologies:</p>
+      <h4>Front end</h4>
+      <ul>
+        <li>React</li>
+        <li>React-icons</li>
+        <li>AOS</li>
+        <li>Styled with CSS only</li>
+      </ul>
+      <h4>Deploy</h4>
+      <ul>
+        <li>Firebase</li>
+      </ul>
+    </article>
+  </div>)
+
+
 export {
   mecGames,
+  weatherApp,
+  waveMusic,
+  myPortfolio,
 }
